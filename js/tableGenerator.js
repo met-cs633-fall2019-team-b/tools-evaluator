bb81a5514f1d990ba30097fb0faf92e9ed3cd399
Devin Dumais <devin@frameworkhomeownership.org>
2019-11-01 16:55:35 -0400

    styles are looking good; check mark toggles on and off

--- js/tableGenerator.js
+++ js/tableGenerator.js
@@ -84,7 +84,12 @@ function evaluateCell(width, height, name) {
     console.log('Evaluating', studentAnswersArray);
     console.log('Evaluate width ', width, ' height ', height, name);
     const indexToRemove = studentAnswersArray.indexOf(orderedToolsArray[height].name + '-' + orderedCategoriesArray[width].id);
-    $('#cell' + width + height).text('\u2713');
+
+    // toggle the check mark
+    ($('#cell' + width + height).text() === '')
+      ? $('#cell' + width + height).text('\u2713')
+      : $('#cell' + width + height).text('');
+
     if (indexToRemove !== -1) {
         // remove it
         console.log('Removing ', indexToRemove);
