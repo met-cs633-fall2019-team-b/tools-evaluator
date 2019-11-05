fca3179ae631d6b15ef55e550b4b1c7cbc6d6a32
Devin Dumais <devin@frameworkhomeownership.org>
2019-11-04 20:55:59 -0500

    add button to view

--- js/tableGenerator.js
+++ js/tableGenerator.js
@@ -7,17 +7,6 @@
  * PR6 Add custom features
  */
 
-const theadOpen = '<thead>';
-const theadClose = '</thead>';
-const tbodyOpen = '<tbody>';
-const tbodyClose = '</tbody>';
-const thOpen = '<th>';
-const thClose = '</th>';
-const trOpen = '<tr>';
-const trClose = '</tr>';
-const tdOpen = '<td>';
-const tdClose = '</td>';
-
 // globals bad but...
 let correctAnswersArray = [];
 let studentAnswersArray = [];
@@ -25,6 +14,17 @@ let orderedCategoriesArray;
 let orderedToolsArray;
 
 function buildTable(params) {
+    const theadOpen = '<thead>';
+    const theadClose = '</thead>';
+    const tbodyOpen = '<tbody>';
+    const tbodyClose = '</tbody>';
+    const thOpen = '<th>';
+    const thClose = '</th>';
+    const trOpen = '<tr>';
+    const trClose = '</tr>';
+    const tdOpen = '<td>';
+    const tdClose = '</td>';
+
     // TODO: Get data via AJAX or fetch API
     let $table = $('<table />');
     let categories = params.categories; // width
